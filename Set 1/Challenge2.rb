7a75bacec9bc7a4d85548dee6251d6221e4de539
C_Sto <cstokes1@our.ecu.edu.au>
2015-12-19 22:40:51 +0800

    Added challenge info to challenge 2

--- Set 1/Challenge2.rb
+++ Set 1/Challenge2.rb
@@ -1,4 +1,17 @@
+=begin
+Fixed XOR
+Write a function that takes two equal-length buffers and produces their XOR combination.
 
+If your function works properly, then when you feed it the string:
+
+1c0111001f010100061a024b53535009181c
+... after hex decoding, and when XOR'd against:
+
+686974207468652062756c6c277320657965
+... should produce:
+
+746865206b696420646f6e277420706c6179
+=end
 #we can just xor these two hex bases
 before = 0x1c0111001f010100061a024b53535009181c
 xorTarget = 0x686974207468652062756c6c277320657965
