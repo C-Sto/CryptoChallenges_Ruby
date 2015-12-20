a3e26926f4abbe72bea76680dde42efd8b9fdd52
C_Sto <cstokes1@our.ecu.edu.au>
2015-12-20 15:22:09 +0800

    Made the main file more informational

--- main.rb
+++ main.rb
@@ -1,12 +1,29 @@
 
+#discover sets
+puts "Sets available:"
+sets = Dir.entries(".")
+#only print set folders that exist
+for x in sets
+  if(x[0..2]=="Set")
+    puts x
+  end
+end
 
-set = ""
-
-chal = ""
-
-puts "please select a set: "
+puts "Please select a set: "
+#chomp to get rid of chars we don't like for printing
 set = gets.chomp
-puts "please select a challenge: "
+#list challenges in selected folder
+puts "Challenges available: "
+dirs = Dir.entries("./Set "+set)
+
+for x in dirs
+  if(x[-1] == 'b')
+    puts "\t"+x[0..-4]
+  end
+end
+
+puts "please select a challenge:"
 chal = gets.chomp
 
+#run the challenge
 system("ruby \"Set "+set+"/Challenge"+chal+".rb\"")
